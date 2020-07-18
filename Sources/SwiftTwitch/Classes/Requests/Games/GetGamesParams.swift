c67c9fa1c0c0971ff978cbb145cc19b39f1530a6
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2020-07-18 14:56:47 +0200

    Add games requests parameters models

--- Sources/SwiftTwitch/Classes/Requests/Games/GetGamesParams.swift
+++ Sources/SwiftTwitch/Classes/Requests/Games/GetGamesParams.swift
@@ -1,8 +1,37 @@
 //
-//  File.swift
-//  
+//  GetGamesParams.swift
+//  SwiftTwitch
 //
 //  Created by Kevin Romero Peces-Barba on 18/07/2020.
 //
 
 import Foundation
+
+/// `GetGamesParams`
+public struct GetGamesParams {
+
+    /// Game ID.
+    ///
+    /// At most 100 id values can be specified.
+    ///
+    /// For a query to be valid, `name` and/or `id` must be specified.
+    let gameIds: [String]?
+    /// Game name.
+    ///
+    /// The name must be an exact match. For instance, “Pokemon” will not return a list of Pokemon games; instead, query the specific Pokemon game(s) in which you are interested.
+    ///
+    /// At most 100 name values can be specified.
+    ///
+    /// For a query to be valid, `name` and/or `id` must be specified.
+    let name: String?
+
+}
+
+extension GetGamesParams: Codable {
+
+    enum CodingKeys: String, CodingKeys {
+        case gameIds = "id"
+        case name = "name"
+    }
+
+}
--- Sources/SwiftTwitch/Classes/Requests/Games/GetTopGamesParams.swift
+++ Sources/SwiftTwitch/Classes/Requests/Games/GetTopGamesParams.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
-//  
+//  GetTopGamesParams.swift
+//  SwiftTwitch
 //
 //  Created by Kevin Romero Peces-Barba on 18/07/2020.
 //
 
 import Foundation
+
+/// `GetTopGamesParams`
+public struct GetTopGamesParams {
+
+    /// Cursor for forward pagination: tells the server where to start fetching the next set of results, in a multi-page response. (optional)
+    ///
+    /// The cursor value specified here is from the pagination response field of a prior query.
+    let after: String?
+    /// Cursor for backward pagination: tells the server where to start fetching the next set of results, in a multi-page response. (optional)
+    ///
+    /// The cursor value specified here is from the pagination response field of a prior query.
+    let before: String?
+    /// Maximum number of objects to return. (optional)
+    ///
+    /// Maximum: 100.
+    ///
+    /// Default: 20.
+    let first: Int?
+
+}
+
+extension GetTopGamesParams: Codable {
+
+    enum CodingKeys: String, CodingKeys {
+        case after = "after"
+        case before = "before"
+        case first = "first"
+    }
+
+}
