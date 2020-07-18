94636eba7cbc8e8c6d836d65712edde9df205e4e
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2020-07-18 16:02:30 +0200

    Update videos requests parameters models

--- Sources/SwiftTwitch/Classes/Requests/Videos/GetVideoParams.swift
+++ Sources/SwiftTwitch/Classes/Requests/Videos/GetVideoParams.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  GetVideoParams.swift
+//  SwiftTwitch
 //
 //  Created by Kevin Romero Peces-Barba on 14/07/2020.
 //
@@ -8,39 +8,75 @@
 import Foundation
 
 /// `GetVideoParams` wraps the necessary parameters to perform a request to get videos
-public struct GetVideoParams: Codable {
+public struct GetVideoParams {
 
-    /// `id`: The IDs of the video to be queried. Maximum 100. If this is specified,
-    /// then no optional values can be used. Optional if `userId` or `gameId` is specified.
-    let id: [String]?
-
-    /// `userId`: The ID of the user to query videos for. Optional if `videoIds` or `gameId`
-    /// is specified.
+    /// ID of the video(s) being queried.
+    ///
+    /// Limit: 100.
+    ///
+    /// If this is specified, you cannot use any of the optional parameters.
+    ///
+    /// Each request must specify one or more video `ids`, one `user_id`, or one `game_id`.
+    let ids: [String]?
+    /// ID of the user who owns the video.
+    ///
+    /// Each request must specify one or more video `ids`, one `user_id`, or one `game_id`.
     let userId: String?
-
-    /// `gameId`: The ID of the game to query videos for. Optional if `videoIds` or `userId`
-    /// is specified.
+    /// ID of the game the video is of.
+    ///
+    /// Each request must specify one or more video `ids`, one `user_id`, or one `game_id`.
     let gameId: String?
 
-    /// `after`: The forward pagination token.
+    /// Cursor for forward pagination: tells the server where to start fetching the next set of results, in a multi-page response. (optional)
+    ///
+    /// The cursor value specified here is from the pagination response field of a prior query.
     let after: String?
-
-    /// `before`: The backwards pagination token.
+    /// Cursor for backward pagination: tells the server where to start fetching the next set of results, in a multi-page response. (optional)
+    ///
+    /// The cursor value specified here is from the pagination response field of a prior query.
     let before: String?
-
-    /// `first`: The maximum number of videos to return. Maximum: 100. Default: 20.
+    /// Number of values to be returned when getting videos by user or game ID. (optional)
+    ///
+    /// Limit: 100.
+    ///
+    /// Default: 20.
     let first: Int?
-
-    /// `language`: The language videos must be in to be returned.
+    /// Language of the video being queried. (optional)
     let language: String?
-
-    /// `period`: The period to obtain data for. If this value is `.all`, Default: `.all`.
+    /// Period during which the video was created. (optional)
+    ///
+    /// Valid values: `.all`, `.day`, `.week`, `.month`.
+    ///
+    /// Default: `.all`.
     let period: Twitch.Videos.Period?
-
-    /// `sortType`: The type of sorting that should occur. Default: `.time`.
+    /// Sort order of the videos. (optional)
+    ///
+    /// Valid values: `.time`, `.trending`, `.views`.
+    ///
+    /// Default: `.time`.
     let sortType: Twitch.Videos.SortType?
-
-    /// `videoType`: The type of videos that should be retrieved. Default: `.all`.
+    /// Type of video. (optional)
+    ///
+    /// Valid values: `.all`, `.upload`, `.archive`, `.highlight`.
+    ///
+    /// Default: `.all`.
     let videoType: Twitch.Videos.VideoQueryType?
+
+}
+
+extension GetVideoParams: Codable {
+
+    enum CodingKeys: String, CodingKey {
+        case ids = "id"
+        case userId = "user_id"
+        case gameId = "game_id"
+        case after = "after"
+        case before = "before"
+        case first = "first"
+        case language = "language"
+        case period = "period"
+        case sortType = "sort"
+        case videoType = "type"
+    }
 
 }
